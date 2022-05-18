dcb866ac49a9aff3852dae07d8da36ceff948545
Denis Tabakov <spbtabakoff@yandex.com>
2022-05-18 11:28:55 +0300

    "add gendiff.py"

---
+++ gendiff.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file', metavar='first_file', help='')
+parser.add_argument('second_file', metavar='second_file', help='')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
