9f00273590ea23b0fe926f32fc31fc0f78b6fa27
Denis Tabakov <spbtabakoff@yandex.com>
2022-05-21 11:22:26 +0300

    "add gendiff.py"

--- gendiff.py
+++ gendiff.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 parser.add_argument('first_file', metavar='first_file', help='')
 parser.add_argument('second_file', metavar='second_file', help='')
+parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
 
 
 if __name__ == '__main__':
