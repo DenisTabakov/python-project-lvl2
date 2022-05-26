3f0d14e62bc84c4e97122f49ada873ce196aeb10
Denis Tabakov <spbtabakoff@yandex.com>
2022-05-26 10:47:16 +0300

    "add gendiff.py"

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -3,10 +3,10 @@ from gendiff import generate_diff
 
 def test_generate_diff(file_1, file_2):
     assert generate_diff(file_1, file_2) == \
-           '{' \
-           '- follow: False host: hexlet.io ' \
-           '- proxy: 123.234.53.22 ' \
-           '- timeout: 50 ' \
-           '+ timeout: 20 ' \
-           '+ verbose: True' \
+           '{\n' \
+           '- follow: False host: hexlet.io\n' \
+           '- proxy: 123.234.53.22\n' \
+           '- timeout: 50\n' \
+           '+ timeout: 20\n' \
+           '+ verbose: True\n' \
            '}'
