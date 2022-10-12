09b06ccdaa571bb22492d358b30e523320a8214d
Denis Tabakov <spbtabakoff@yandex.com>
2022-10-12 17:45:14 +0300

    modified tests

---
+++ tests/test_json_full_yaml.py
@@ -0,0 +1,23 @@
+from engine.generate_diff import get_diff
+from formaters.j_son import convert
+import os
+import yaml
+from yaml.loader import SafeLoader
+
+
+file_1 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s1.yaml')
+file_2 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s2.yaml')
+file_3 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_json_full.txt')
+
+with open(file_1) as f_1:
+    data_1 = yaml.load(f_1, Loader=SafeLoader)
+
+with open(file_2) as f_2:
+    data_2 = yaml.load(f_2, Loader=SafeLoader)
+
+with open(file_3) as f_3:
+    ref_diff = f_3.read()
+
+
+def test_plan():
+    assert convert(get_diff(data_1, data_2)) == ref_diff
