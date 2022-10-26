c04958917f0932a5de586e6a04a8b556c9a1f048
Denis Tabakov <spbtabakoff@yandex.com>
2022-10-26 17:55:41 +0300

    modified tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,4 +1,4 @@
-from gendiff.generate_diff import get_diff
+from gendiff.engine.generate_diff import get_diff
 from gendiff.formaters import stylish, j_son, plain
 from gendiff.parser import parse
 import os
@@ -10,6 +10,8 @@ file_3 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s1.json')
 file_4 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s2.json')
 file_5 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s1.yaml')
 file_6 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s2.yaml')
+file_51 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s1.yml')
+file_61 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_s2.yml')
 file_7 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_stylish.txt')
 file_8 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_stylish_full.txt')
 file_9 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file_plain_full.txt')
@@ -21,6 +23,8 @@ data_3 = parse(file_3)
 data_4 = parse(file_4)
 data_5 = parse(file_5)
 data_6 = parse(file_6)
+data_51 = parse(file_51)
+data_61 = parse(file_61)
 diff_stylish_small = parse(file_7)
 diff_stylish_full = parse(file_8)
 diff_plain_full = parse(file_9)
@@ -29,6 +33,9 @@ diff_json_full = parse(file_10)
 
 def test_parse():
     assert data_3 == data_5
+    assert data_4 == data_6
+    assert data_3 == data_51
+    assert data_4 == data_61
 
 
 def test_stylish():
