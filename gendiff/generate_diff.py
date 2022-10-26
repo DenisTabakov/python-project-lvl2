dbd0468a9fdedba98daf835e47560a56482c2903
Denis Tabakov <spbtabakoff@yandex.com>
2022-10-26 18:23:50 +0300

    modified files

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,20 +1,22 @@
 import os
-from formaters import stylish, j_son, plain
-from parser import choose_format
+from gendiff.formaters import stylish, j_son, plain
+from gendiff.parser import parse
 
 OUT_FORMATS = {'stylish': stylish.convert, 'plain': plain.convert, 'json': j_son.convert}
 
 
-def generate_diff():
-    data_1, data_2, output_format, output_extension = choose_format()
+def generate_diff(file_1, file_2, output_format):
+    data_1 = parse(file_1)
+    data_2 = parse(file_2)
+    # output_extension = choose_format()
     result = OUT_FORMATS[output_format](get_diff(data_1, data_2))
-
+    '''
     if output_extension != "None":
         path_1 = os.path.join(os.getcwd(), 'output.')
         output_path = f"{path_1}{str(output_extension)}"
         with open(output_path, "w") as output_file:
             output_file.write(result)
-
+    '''
     return result
 
 
@@ -40,4 +42,3 @@ def get_diff(data_1, data_2):
             value = [["value", val_1], ["value", val_2]]
         result[flag, key] = value
     return result
-
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from gendiff.generate_diff import generate_diff
+from gendiff.parser import choose_format
 
 
 def main():
-    diff = generate_diff()
+    file_1, file_2, output_format = choose_format()
+    diff = generate_diff(file_1, file_2, output_format)
     print(diff)
 
 
